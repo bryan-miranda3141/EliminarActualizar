4002126c4a9995246402f3647cebf0be5c9852b6
Docente <Docente@10.20.0.176>
2023-06-15 07:50:39 -0600

    Subi mi proyecto

---
+++ src/archivos/ConexionSQL.java
@@ -0,0 +1,30 @@
+
+package archivos;
+
+import java.sql.*;
+
+public class ConexionSQL {
+    
+    
+    public static Connection getConexion(){
+        
+     
+        String conexionUrl  = "jdbc:sqlserver://localhost:58075;" +
+     "databaseName=UsersDB;"
+                + "user=sa;"
+                + "password=itr2023;" +
+     "encrypt=true;trustServerCertificate=true";
+        
+        
+        try{
+            Connection conn = DriverManager.getConnection(conexionUrl) ;
+            return conn;
+        }catch(SQLException ex){
+            System.out.println(ex.toString());
+  
+            return null;
+        }
+        
+    }
+    
+}
